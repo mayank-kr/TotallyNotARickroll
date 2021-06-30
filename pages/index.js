843b317bc518e7c0f89659897dbd61da5ffc78b2
Mayank Kumar <kmayank38@gmail.com>
2021-06-30 20:53:38 +0530

    modified index.js

--- pages/index.js
+++ pages/index.js
@@ -4,59 +4,21 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Why not to join BLAHAJGang</title>
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome! Today I will answer the biggest question.
         </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h2 className="question">
+          Why you should not join BLAHAJGang🦈?
+        </h2>
+        <a className="btn" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Click here to know the answer</a>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
+          Developed by <a href="http://github.com/mayank-kr" target="_blank" rel="noreferrer"> Mayank Kumar</a>
       </footer>
 
       <style jsx>{`
@@ -78,6 +40,22 @@ export default function Home() {
           align-items: center;
         }
 
+        .btn {
+          font-size: 1.2rem;
+          padding:10px;
+          margin: 10px;
+          background-color: white;
+          color: black;
+          border-radius: 8px;
+          border: 5px solid #0070f3;
+        }
+
+        .btn:hover{
+          cursor: pointer;
+          background-color: #0070f3;
+          color: white;
+        }
+
         footer {
           width: 100%;
           height: 100px;
@@ -85,10 +63,6 @@ export default function Home() {
           display: flex;
           justify-content: center;
           align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
         }
 
         footer a {
@@ -102,26 +76,26 @@ export default function Home() {
           text-decoration: none;
         }
 
-        .title a {
+        a {
           color: #0070f3;
           text-decoration: none;
+          padding:5px;
         }
 
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
+        .title, .question {
           margin: 0;
           line-height: 1.15;
           font-size: 4rem;
+          padding: 20px;
         }
 
         .title,
-        .description {
+        .description, .question {
           text-align: center;
+        }
+
+        .question{
+          color: #0070f3;
         }
 
         .description {
@@ -136,50 +110,6 @@ export default function Home() {
           font-size: 1.1rem;
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
         }
 
         @media (max-width: 600px) {
